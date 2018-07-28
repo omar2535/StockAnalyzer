d68204746ee0080262febba8dd1bb3f537a21031
Omar <omar2535@gmail.com>
2018-07-27 22:11:22 -0700

    Completed scraping for current data

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,6 +1,22 @@
+#to get params from form, use params[:field].inspect
+#
+
 class StocksController < ApplicationController
     def new
-    
     end
 
+    def create
+      render plain: "stuff"
+      scraper = StocksHelper::StockScraper.new(stock_search_params[:stockName])
+    end
+
+    def index
+
+    end
+
+
+  private
+    def stock_search_params
+      params.require(:stock).permit(:stockName)
+    end
 end
--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -1,2 +1,53 @@
+require 'nokogiri'
+require 'open-uri'
+require 'openssl'
+#To prevent openssl errors
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
+
 module StocksHelper
+
+  class StockScraper
+    attr_reader :stockName
+    def initialize(stockName)
+      puts "Stock name to search for is: " + stockName
+      @stockName = stockName
+      scrape
+    end
+
+    #top caller of scraping all data
+    #will return all scraped data and perform assesment
+    def scrape()
+      scrapeForCurrentData
+    end
+
+    private
+      #to scrape for current intraday data
+      def scrapeForCurrentData()
+        url = "https://ca.finance.yahoo.com/quote/" + @stockName + "/key-statistics?"
+        result = {}
+        puts url
+        page = Nokogiri::HTML(open(url))
+        main_contents = page.css('tr td')
+        main_contents.search('sup').remove
+        main_contents.each_with_index do |content, index|
+          if index%2 == 0
+            result[content.text] = main_contents[index+1].text
+          end
+        end
+        #puts result
+        return result
+      end
+
+
+
+
+
+      #scrape for cashflow
+      def scrapeForBalanceSheet()
+
+      end
+  end
+
+
 end
